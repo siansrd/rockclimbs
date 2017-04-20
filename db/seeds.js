5c4c71c396bc97a059742ac29968dff8d78c0789
siansrd <sianrobinsondavies@gmail.com>
2017-04-20 15:16:26 +0100

    Update seeds for database

--- db/seeds.js
+++ db/seeds.js
@@ -1,10 +1,10 @@
 use rockclimbing;
-db.dropDatabase()
+db.dropDatabase();
 
 db.crags.insert([
   { 
     name: "Hawkcraig",
-    latlng: {56.05028,-3.28583},
+    latlng: "56.05028, -3.28583",
     climbs: [
       {name: "Pain Pillar", grade: "VS 4c"},
       {name: "Asinine", grade: "VS 5a"}
@@ -12,7 +12,7 @@ db.crags.insert([
   },
   { 
     name: "Limekilns",
-    latlng: {56.03720,-3.49420},
+    latlng: "56.03720, -3.49420",
     climbs: [
       {name: "Cruel Summer", grade: "E2 5c"},
       {name: "Forbidden Colours", grade: "HVS 5a"}
